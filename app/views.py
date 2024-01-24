c99f576125ea1a673383570da0dfe0752d67ea3e
Matheus Filg <matheus_falmeida@hotmail.com>
2024-01-24 19:20:08 -0300

    Adicionando Rotas

--- app/views.py
+++ app/views.py
@@ -1,6 +1,10 @@
 from app import app
-from flask import render_template
+from flask import render_template, url_for
 
 @app.route('/')
 def homepage():
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return 'Olá, me chamo fulaninho e sou estudante :)'
