f69e6c74632ea225a5305ed8190e3b40ac12036c
Matheus Filg <matheus_falmeida@hotmail.com>
2024-01-25 20:04:07 -0300

    Create List of Contacts

--- app/views.py
+++ app/views.py
@@ -18,3 +18,17 @@ def contact():
         return redirect(url_for('homepage'))
     
     return render_template('contact.html', context=context, form=form)
+
+
+@app.route('/contact/list/')
+def contact_list():
+
+    if request.method == 'GET':
+        search = request.args.get('search', '')
+    data = Contato.query.order_by('name')
+    if search != '':
+        data = data.filter_by(name=search)
+
+    context = {'data': data.all()}
+
+    return render_template('contact_list.html', context=context)
