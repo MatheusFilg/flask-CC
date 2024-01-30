9c8a1a568d971bdc70785e75a43318343efee3be
Matheus F <matheus_falmeida@hotmail.com>
2024-01-30 11:17:56 -0300

    Changing Envs

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
+
+load_dotenv('.env')
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'IOSAJFIOASHFAUISEFHWQ-UIQWH13123412'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 
 db = SQLAlchemy(app)
